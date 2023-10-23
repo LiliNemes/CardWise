e9389a4d0568347e212db4329a9a8fa16e7fdb60
LiliNemes <lili.nemes@edu.bme.hu>
2023-10-23 15:57:14 +0200

    Splash screen and base of login screen

---
+++ CardWise/app/src/main/java/hu/bme/aut/android/cardwise/LoginActivity.kt
@@ -0,0 +1,13 @@
+package hu.bme.aut.android.cardwise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
